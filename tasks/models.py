2bd9b5df36c8168a84949815137b0c9a1a0337ad
HomeBjo <lala2-wuppertal@hotmail.com>
2024-07-15 16:43:04 +0200

    $*

--- tasks/models.py
+++ tasks/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
     description = models.TextField(blank=True)  
     due_date = models.DateField(null=True, blank=True) 
     priority = models.CharField(max_length=50, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium') 
-    assign_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tasks')  
+    assign_to = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='assigned_tasks')
     category = models.CharField(max_length=100, blank=True) 
     created_at = models.DateField(default=date.today)  
     author = models.ForeignKey(
@@ -19,6 +19,7 @@ class Task(models.Model):
     
     def __str__(self):
         return f'({self.id}) {self.title}'
+
 class Subtask(models.Model):
     task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
     value = models.CharField(max_length=255)
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from tasks.models import Task, Subtask
 from django.contrib.auth.models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+
 class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
@@ -20,36 +20,47 @@ class SubtaskSerializer(serializers.ModelSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
     subtasks = SubtaskSerializer(many=True, required=False)
-    assign_to = UserSerializer(read_only=True)
-    assign_to_id = serializers.PrimaryKeyRelatedField(
-    queryset=User.objects.all(), source='assign_to', write_only=True, required=False
-    )
-    
+    assign_to = UserSerializer(many=True, read_only=True)
+    assign_to_ids = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, write_only=True, required=False)
+
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'due_date', 'priority', 'assign_to','assign_to_id', 'category', 'created_at', 'author', 'subtasks']
+        fields = ['id', 'title', 'description', 'due_date', 'priority', 'assign_to', 'assign_to_ids', 'category', 'created_at', 'author', 'subtasks']
         extra_kwargs = {
-            'author': {'required': False} 
+            'author': {'required': False}
         }
-        
+
     def create(self, validated_data):
         subtasks_data = validated_data.pop('subtasks', [])
+        assign_to_ids = validated_data.pop('assign_to_ids', [])
         request = self.context.get('request')
         validated_data['author'] = request.user
         task = Task.objects.create(**validated_data)
+        task.assign_to.set(assign_to_ids)
         for subtask_data in subtasks_data:
             Subtask.objects.create(task=task, **subtask_data)
         return task
-    
+
     def update(self, instance, validated_data):
-       
+        subtasks_data = validated_data.pop('subtasks', [])
+        assign_to_ids = validated_data.pop('assign_to_ids', [])
 
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description', instance.description)
         instance.due_date = validated_data.get('due_date', instance.due_date)
         instance.priority = validated_data.get('priority', instance.priority)
-        instance.assign_to = validated_data.get('assign_to', instance.assign_to)
         instance.category = validated_data.get('category', instance.category)
+        instance.assign_to.set(assign_to_ids)
         instance.save()
-        
+
+        for subtask_data in subtasks_data:
+            subtask_id = subtask_data.get('id')
+            if subtask_id:
+                subtask = Subtask.objects.get(id=subtask_id, task=instance)
+                subtask.value = subtask_data.get('value', subtask.value)
+                subtask.status = subtask_data.get('status', subtask.status)
+                subtask.save()
+            else:
+                Subtask.objects.create(task=instance, **subtask_data)
+
         return instance
